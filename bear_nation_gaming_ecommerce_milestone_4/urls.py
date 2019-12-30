0caa47519646cd3f61f5fd5c8efdddca4daec457
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2019-12-30 20:42:33 +0000

    created urls for the other apps in the project

--- bear_nation_gaming_ecommerce_milestone_4/urls.py
+++ bear_nation_gaming_ecommerce_milestone_4/urls.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('products.urls')),
+    path('products/', include('products.urls')),
+    # path('blogs/', include('blogs.urls')),
+    # path('reviews/', include('reviews.urls')),
+    # path('cart/', include('cart.urls')),
+    # path('checkout/', include('checkout.urls')),
 ]
