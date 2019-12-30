464cd8073cf826e662a122717db150d544ba88ea
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2019-12-30 20:57:30 +0000

    created the models for the reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+from users.models import User
+
+
+class Review(models.Model):
+	game_title = models.CharField(max_length=254, default='')
+	author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+	review = models.TextField()
+	score = models.IntegerField()
+	image = models.ImageField(upload_to='images')
