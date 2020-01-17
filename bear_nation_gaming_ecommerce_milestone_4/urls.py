c15f8d03c0cef5a084682ea22fbf9007abd6d24b
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-17 18:02:29 +0000

    Updated the projects urls.py file to include the new updated accounts app urls, also have started to include the products and reviews urls

--- bear_nation_gaming_ecommerce_milestone_4/urls.py
+++ bear_nation_gaming_ecommerce_milestone_4/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.views.static import serve
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('products/', include('products.urls')),
     # path('blogs/', include('blogs.urls')),
-    # path('reviews/', include('reviews.urls')),
+     path('reviews/', include('reviews.urls')),
     # path('cart/', include('cart.urls')),
     # path('checkout/', include('checkout.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
