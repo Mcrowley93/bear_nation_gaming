d89df6eae227ce7ab2786289cab06d678c8b8b74
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-18 14:48:39 +0000

    Imported Product from the models, then created the view for all_products, this renders in the all_products.html web page and allows for all the products in the database to be displayed

--- products/views.py
+++ products/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Product
+from django.contrib.admin.views.decorators import staff_member_required
 
-def home(request):
-    welcome = "Hello World!"
-    return render (request, 'products/home.html', {'message': welcome})
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'products/all_products.html', {"products": products})
+
+# @staff_member_required
