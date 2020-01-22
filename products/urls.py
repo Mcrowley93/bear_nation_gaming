06db44e27296a8488e87289643952a79ef74f48e
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-22 20:04:10 +0000

    able to access the products page and also created a urlpattern for the search function featured on the products page

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
-from products.views import all_products
+from products.views import all_products, products_search
 # , product_details
 
 urlpatterns = [
     path('products/', all_products, name='all_products'),
     # path('product_details/', product_details, name='product_details'),
+    path('products_search', products_search, name='products_search'),
 ]
