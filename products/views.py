91333dc40d5467027a7d93514162c5129a85d5ce
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-22 20:06:04 +0000

    all_products page shows all the products currently listed in the database. Also has a search function so the user can search through the items in the products database by name

--- products/views.py
+++ products/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Product
-from django.contrib.admin.views.decorators import staff_member_required
 
 
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'products/all_products.html', {"products": products})
 
-# @staff_member_required
+
+def products_search(request):
+    products = Product.objects.filter(name__icontains=request.GET['query'])
+    return render(request, "products/all_products.html", {"products": products})
