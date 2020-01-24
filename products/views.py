7a024f22ca2aa506af5cfd9de7481d0e586b853b
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-24 22:39:12 +0000

    Reworked code for the products app views.py file, including the products_search function, which allows users to filter the products on the product page by name

--- products/views.py
+++ products/views.py
@@ -7,6 +7,7 @@ def all_products(request):
     return render(request, 'products/all_products.html', {"products": products})
 
 
+# products_search function is case insensitive and gets the 'query' and filters the Products based on name
 def products_search(request):
     products = Product.objects.filter(name__icontains=request.GET['query'])
     return render(request, "products/all_products.html", {"products": products})
