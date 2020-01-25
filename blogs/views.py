6f83cf71c64510b4de144c7c5da8f829f66f15d2
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-25 14:14:39 +0000

    created the post_detail view, this uses a get_object_or_404 to get a post with the same pk as the one that is passed from the urlpattern

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
 
 def all_posts(request):
-    return render(request, 'blogs/all_posts.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, "blogs/all_posts.html", {"posts": posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogs/post_detail.html', {'post': post})
+
+
+# posts_search function is case insensitive and gets the 'query' and filters the Posts based on name
+def posts_search(request):
+    posts = Post.objects.filter(title__icontains=request.GET['query'])
+    return render(request, "blogs/all_posts.html", {"posts": posts})
