2a910acf63c97cd5510e6a00b14e1731771d00b5
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-25 14:12:26 +0000

    created the urlpattern for the post_detail view, it takes an integer value and passes it to the post_detail view as a variable called pk

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from blogs.views import all_posts
+from blogs.views import all_posts, post_detail, posts_search
 
 urlpatterns = [
-    path('blogs/', all_posts, name='all_posts'),
+    path('all_posts/', all_posts, name='all_posts'),
+    # the post_detail urlpattern begins with post/ we then use <int:pk>/ so that django expects an integer value
+    # it will then transfer it to the post_detail view as a variable called pk
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('posts_search', posts_search, name='posts_search'),
+
 ]
