aa71ec47b7cac9ba776d69945feef49ae9fbe192
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-28 16:28:45 +0000

    changed the models.py file to contain a CharField for platform, therefore allowing users to know if the review they are reading is relevant to their console of choice

--- reviews/models.py
+++ reviews/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from accounts.models import User
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Review(models.Model):
 	game_title = models.CharField(max_length=254, default='')
 	author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+	created_date = models.DateTimeField(default=timezone.now)
+	platform = models.CharField(max_length=254, default='')
 	review = models.TextField()
-	score = models.IntegerField()
-	image = models.ImageField(upload_to='images')
+	score = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
