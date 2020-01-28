1b7f0895efb5455bc9ac6c949abb802f5b6f017f
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-28 01:39:15 +0000

    added login_required to the blogs views, to ensure that users that havent registered, arent able to access the blogs pages

--- blogs/views.py
+++ blogs/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def all_posts(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, "blogs/all_posts.html", {"posts": posts})
 
 
+@login_required()
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -23,6 +26,7 @@ def post_new(request):
     return render(request, 'blogs/post_edit.html', {'form': form})
 
 
+@login_required()
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,12 +42,14 @@ def post_edit(request, pk):
     return render(request, 'blogs/post_edit.html', {'form': form})
 
 
+@login_required()
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
 
 
+@login_required()
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blogs/post_detail.html', {'post': post})
