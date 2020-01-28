d066f762027e43e5861bcaccbf6ffac33d4b096c
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-28 01:40:07 +0000

    created the urlpattern for the product_details view

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from products.views import all_products, products_search
-# , product_details
+from products.views import all_products, products_search, product_details
 
 urlpatterns = [
-    path('products/', all_products, name='all_products'),
-    # path('product_details/', product_details, name='product_details'),
+    path('all_products/', all_products, name='all_products'),
+    path('product_details/<int:pk>/', product_details, name='product_details'),
     path('products_search', products_search, name='products_search'),
 ]
