4d564c9edd67a3ccacde9a9932576f009af27299
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-28 16:47:54 +0000

    sorted the urlpatterns for the all_reviews view and the add_a_review view

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from reviews.views import all_reviews, add_a_review, reviews_search
 
 urlpatterns = [
-    path('all_reviews/', views.reviews, name='all_reviews'),
-
+    path('all_reviews/', all_reviews, name='all_reviews'),
+    path('add_a_review/', add_a_review, name='add_a_review'),
+    path('reviews_search', reviews_search, name='reviews_search'),
 ]
