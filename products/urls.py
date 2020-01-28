31007da1c4b8bfbc3e18d9921e2b30ca890ee7c6
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-01-28 13:04:49 +0000

    created the urlpattern for the add_review_to_product view

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from products.views import all_products, products_search, product_details
+from products.views import all_products, products_search, product_details, add_review_to_product
 
 urlpatterns = [
     path('all_products/', all_products, name='all_products'),
     path('product_details/<int:pk>/', product_details, name='product_details'),
     path('products_search', products_search, name='products_search'),
+    path('product/<int:pk>/review/', add_review_to_product, name='add_review_to_product'),
+
 ]
