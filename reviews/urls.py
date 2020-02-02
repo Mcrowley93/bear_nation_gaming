f412b2aa1a9fc0b1c553cb4daeac4cbd7c76463c
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-02-02 10:17:06 +0000

    updated the urls.py file for the review app, so the urls now exist for each view/function created (add, edit, remove, read, etc)

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from reviews.views import all_reviews, add_a_review, reviews_search
+from reviews.views import all_reviews, add_a_review, reviews_search, read_review, edit_a_review, remove_review
 
 urlpatterns = [
     path('all_reviews/', all_reviews, name='all_reviews'),
     path('add_a_review/', add_a_review, name='add_a_review'),
     path('reviews_search', reviews_search, name='reviews_search'),
+    path('read_review/<int:pk>/', read_review, name='read_review'),
+    path('review/<int:pk>/edit/', edit_a_review, name='edit_a_review'),
+    path('review/<pk>/remove/', remove_review, name='remove_review'),
+
 ]
