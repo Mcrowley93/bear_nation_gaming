130c9de661e2cf848b34f36080fb3f5b8af493d1
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-02-02 10:19:14 +0000

    changed the views for add_a_review and edit_a_review, so they now use their dedicated templates

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Review
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -30,7 +30,42 @@ def add_a_review(request):
     return render(request, 'reviews/add_a_review.html', {"form": form})
 
 
+@login_required()
+def edit_a_review(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.user != review.author:
+        messages.error(request, "You can only edit the reviews you have written!")
+        return redirect('all_reviews')
+    if request.method == "POST":
+        form = AddReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.author = request.user
+            review.save()
+
+            messages.success(request, "You have successfully edited your review!")
+            return redirect('read_review', pk=review.pk)
+    else:
+        form = AddReviewForm(instance=review)
+    return render(request, 'reviews/edit_a_review.html', {'form': form})
+
+
 # reviews_search function is case insensitive and gets the 'query' and filters the Products based on game_title
 def reviews_search(request):
     reviews = Review.objects.filter(game_title__icontains=request.GET['query'])
     return render(request, "reviews/reviews.html", {"reviews": reviews})
+
+
+def read_review(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    return render(request, 'reviews/read_review.html', {'review': review})
+
+
+@login_required()
+def remove_review(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.user != review.author:
+        messages.error(request, "You can only delete the reviews you have submitted!")
+        return redirect('all_reviews')
+    review.delete()
+    return redirect('all_reviews')
